0cae07cb474575151424941207d5cbd75868dd62
vbroussard <vbroussard@users.noreply.github.com>
2013-06-24 07:41:16 +0000

    WSSODATABASE-20 : correction of SQL update script.
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/authentication/module-mylutece-wssodatabase@49141 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/mylutece/modules/wssodatabase/upgrade/update_db_mylutece-wssodatabase-2.2.1-2.2.2.sql
+++ src/sql/plugins/mylutece/modules/wssodatabase/upgrade/update_db_mylutece-wssodatabase-2.2.1-2.2.2.sql
@@ -1,4 +1,5 @@
 --
 -- UPDATE of email size to 256 for WSSO constraints
 --
-ALTER TABLE mylutece_wsso_user MODIFY email  varchar(256) default '' NOT NULL; 
+ALTER TABLE mylutece_wsso_user MODIFY email  varchar(256) default '' NOT NULL;
+ALTER TABLE mylutece_wsso_user ADD COLUMN date_last_login date NULL;
