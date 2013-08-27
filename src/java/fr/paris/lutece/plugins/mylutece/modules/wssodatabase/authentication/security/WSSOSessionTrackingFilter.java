886b45653315679028eb279dff5914c67ab503c5
FrancoisEricMerlin <FrancoisEricMerlin@users.noreply.github.com>
2013-08-27 09:59:51 +0000

    Add Filter for checking if the user logged in SSO session corresponding to the user stored in application session
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/authentication/module-mylutece-wssodatabase@51143 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/java/fr/paris/lutece/plugins/mylutece/modules/wssodatabase/authentication/security/WSSOSessionTrackingFilter.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2002-2013, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.plugins.mylutece.modules.wssodatabase.authentication.security;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+
+import fr.paris.lutece.portal.service.security.LuteceUser;
+import fr.paris.lutece.portal.service.security.SecurityService;
+import fr.paris.lutece.portal.service.util.AppPropertiesService;
+
+
+/**
+ * Filter Used for checking if the user logged in
+ * SSO session corresponding to the user stored in application session
+ */
+public class WSSOSessionTrackingFilter implements Filter
+{
+
+    private static final String PROPERTY_COOKIE_WSSOGUID = "mylutece-wssodatabase.cookie.wssoguid"; // unique hexa user id
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void init( FilterConfig config ) throws ServletException
+    {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void destroy(  )
+    {
+        // Do nothing
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void doFilter( ServletRequest request, ServletResponse response, FilterChain chain )
+        throws IOException, ServletException
+    {
+        HttpServletRequest req = (HttpServletRequest) request;
+        LuteceUser user= SecurityService.getInstance().getRegisteredUser(req);
+        
+        if(user != null)
+        {
+	        Cookie[] cookies = req.getCookies();
+			String strUserID = null;
+	
+			if ( cookies != null )
+			{
+				for ( int i = 0; i < cookies.length; i++ )
+				{
+					Cookie cookie = cookies[i];
+	
+					if ( cookie.getName( ).equals( AppPropertiesService.getProperty( PROPERTY_COOKIE_WSSOGUID ) ) )
+					{
+						strUserID = cookie.getValue( );
+						if(!StringUtils.isEmpty(strUserID) && !strUserID.equals(user.getName()))
+						{
+							
+							SecurityService.getInstance().unregisterUser(req);
+							
+						}
+						
+						break;
+						
+					}
+				}
+			}
+        }
+			
+       
+        chain.doFilter( request, response );
+    }
+
+    
+}
