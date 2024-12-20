bfa4dcf372b1ce47ea9898d18b64699cc88652ea
OUGHDI <mustapha.oughdi@paris.fr>
2024-12-20 11:21:36 +0100

    LUT-29164 : Fix NoClassDefFoundError with updating the version of apache-commons-lang

--- src/java/fr/paris/lutece/plugins/mylutece/modules/wssodatabase/authentication/service/WssoDatabaseService.java
+++ src/java/fr/paris/lutece/plugins/mylutece/modules/wssodatabase/authentication/service/WssoDatabaseService.java
@@ -41,7 +41,7 @@ import java.util.Map;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import fr.paris.lutece.plugins.mylutece.authentication.MultiLuteceAuthentication;
 import fr.paris.lutece.plugins.mylutece.modules.wssodatabase.authentication.IdxWSSODatabaseAuthentication;
